d9ac910f8d1b88d4c6a29231f6a74a9ee2d69cf0
Ian Salazar <iansalazar@Ians-MacBook-Pro.local>
2017-11-15 11:16:30 -0500

    Fixing .travis file variable mapping issue.

--- xlr/deploy_votingapp.sh
+++ xlr/deploy_votingapp.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 CREDENTIALS=$1
-CREDENTIALS+=:
-CREDENTIALS+=$2
-VOTE_DEPLOY_PATH=$3
-RELEASE_TITLE=$4
+VOTE_DEPLOY_PATH=$2
+RELEASE_TITLE=$3
 
 curl -u $CREDENTIALS -v -H "Content-Type: application/json" $VOTE_DEPLOY_PATH -i -X POST -d '{"releaseTitle": "Deploy Voting App Multi tier", "releaseVariables": {"${user}": "iasalazar"}}' 
