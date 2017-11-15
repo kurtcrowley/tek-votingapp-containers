6020c755ea81c29841eeebbd9a842ece6b48400d
Ian Salazar <iansalazar@Ians-MacBook-Pro.local>
2017-11-15 12:40:52 -0500

    Now passing release title and adjusted results title and result item names

--- xlr/deploy_votingapp.sh
+++ xlr/deploy_votingapp.sh
@@ -4,4 +4,4 @@ CREDENTIALS=$1
 VOTE_DEPLOY_PATH=$2
 RELEASE_TITLE=$3
 
-curl -u $CREDENTIALS -v -H "Content-Type: application/json" $VOTE_DEPLOY_PATH -i -X POST -d '{"releaseTitle": "Deploy Voting App Multi tier", "releaseVariables": {"${user}": "iasalazar"}}' 
+curl -u $CREDENTIALS -v -H "Content-Type: application/json" $VOTE_DEPLOY_PATH -i -X POST -d '{"releaseTitle": "'$RELEASE_TITLE'", "releaseVariables": {"${user}": "iasalazar"}}' 
